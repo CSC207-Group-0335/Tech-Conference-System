0ee88e50dd7e6f32df99a13dad738e5a5ec50ab0
Nathan DeGoey <n.degoey@mail.utoronto.ca>
2020-11-11 18:02:03 -0500

    added javadoc to user class

--- phase1/src/UserLogin/User.java
+++ phase1/src/UserLogin/User.java
@@ -1,9 +1,21 @@
 package UserLogin;
 
+/**
+ * An abstract class that represents a User.
+ */
+
 public abstract class User {
     private String name;
     private String password;
     private String email;
+
+    /**
+     * A user is identified by a name, password and email address (the email will be used as a unique identifier.
+     * @param name the name of the user.
+     * @param password the password of the user.
+     * @param email a unique string representing the email address of the user that they have used to
+     *              register for the conference.
+     */
 
     public User(String name, String password, String email){
         this.name = name;
