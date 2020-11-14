2f4a277df8c770e9547eee4dd0b276473879e067
Nathan DeGoey <n.degoey@mail.utoronto.ca>
2020-11-14 02:19:54 -0500

    Major updates in MainMenuController and MainMenuPresenter

--- phase1/src/UserLogin/MainMenuPresenter.java
+++ phase1/src/UserLogin/MainMenuPresenter.java
@@ -7,65 +7,8 @@ package UserLogin;
 
 public class MainMenuPresenter {
 
-    public void printMainMenuInfoAttendee(int i) {
-        switch (i) {
-            case 0: {
-                System.out.println("Welcome to our Tech Conference. This is the main menu. Navigate to different " +
-                        "screens by typing \"1 -> Conference Schedule\", \"2 -> Personal Schedule\", " +
-                        "\"3 -> Sign up for a Talk\", \"4 -> Cancel Talk registration\",  \"5 -> New Message\" " +
-                        "\"View Messages\"");
-                break;
-            }
-            case 1: {
-                System.out.println("Here is the current Conference Schedule: ");
-                break;
-            }
-            case 2: {
-                System.out.println("Here is your Personal Conference Schedule");
-                break;
-            }
-            case 3: {
-                System.out.println("Which Talk would you like to sign up for?");
-                break;
-            }
-            case 4: {
-                System.out.println("Which Talk would you like to cancel?");
-                break;
-            }
-            case 5: {
-                System.out.println("Please specify the user that you would like to message: ");
-                break;
-            }
-            case 6: {
-                System.out.println("Here are all your current messages: ");
-                break;
-            }
-        }
-    }
-
-    public void printMainMenuInfoSpeaker(int i) {
-        if (i == 1) {
-            System.out.println("Welcome to our Tech Conference. This is the main menu. Navigate to different screens" +
-                    "by typing \"Messages\", \"Schedule\" ");
-        } else if (i == 2) {
-            System.out.println("Enter your password: ");
-        } else if (i == 3) {
-            System.out.println("Login Successful");
-        } else if (i == 4) {
-            System.out.println("Email and password combination not found, try again.");
-        }
-    }
-
-    public void printMainMenuInfoOrganizer(int i) {
-        if (i == 1) {
-            System.out.println("Welcome to our Tech Conference. This is the main menu. Navigate to different screens" +
-                    "by typing \"Messages\", \"Schedule\" ");
-        } else if (i == 2) {
-            System.out.println("Enter your password: ");
-        } else if (i == 3) {
-            System.out.println("Login Successful");
-        } else if (i == 4) {
-            System.out.println("Email and password combination not found, try again.");
-        }
+    public void printMainMenuInfo() {
+        System.out.println("Welcome to our Tech Conference. This is the main menu. Navigate to different " +
+                "screens by typing: \n\"1 -> Schedules\" \n\"2 -> Messages\""); //\n equals new line
     }
 }
