e2754d04dd53b06cc12ff0595d1c306749134ba6
Jolie Zhou <jolie.zhou@mail.utoronto.ca>
2020-11-13 19:46:47 -0500

    modified MessagingSystem

--- phase1/src/MessagingPresenters/MessagingSystem.java
+++ phase1/src/MessagingPresenters/MessagingSystem.java
@@ -5,5 +5,30 @@ package MessagingPresenters;
  */
 
 public class MessagingSystem {
+    public ConversationStorage conversationStorage;
+    public MessengerController messengerController; // ?
     public SpeakerMessengerController speakerMessengerController;
+    public OrganizerMessengerController organizerMessengerController;
+
+    /**
+     * Instantiates OrganizerMessengerController, SpeakerMessengerController, MessengerController, and
+     * ConversationStorage.
+     */
+
+    public MessagingSystem() {
+        // parameters?
+        this.organizerMessengerController = new OrganizerMessengerController();
+        this.speakerMessengerController = new SpeakerMessengerController();
+        //this.messengerController = new MessengerController();
+        this.conversationStorage = new ConversationStorage();
+    }
+
+    /**
+     * Requests conversationStorage to create a ConversationManager.
+     */
+
+    public void createConversationManager(){
+        //request conversationStorage to create conversation manager
+    }
 }
+
