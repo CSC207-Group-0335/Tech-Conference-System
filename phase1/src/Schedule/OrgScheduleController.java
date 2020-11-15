2d11e2cc9be691751252a0530dfe36362b872780
arifaulakh <arifaulakh@gmail.com>
2020-11-15 03:18:37 -0500

    add HashMap and UserStorage variables to OrgScheduleController

--- phase1/src/Schedule/OrgScheduleController.java
+++ phase1/src/Schedule/OrgScheduleController.java
@@ -3,28 +3,28 @@ package Schedule;
 import UserLogin.*;
 
 import java.time.LocalDateTime;
-import java.util.Date;
-
-import java.util.ArrayList;
-import java.util.Observable;
-import java.util.Observer;
+import java.util.*;
 
 public class OrgScheduleController extends UserScheduleController implements Observer {
     UserScheduleManager organizer;
     TalkManager talkManager;
     RoomStorage roomStorage;
+    UserStorage userStorage;
     MainMenuController mainMenuController;
+    HashMap<Talk, SignUpAttendeesManager> signUpAttendeesManager;
 
 
     public OrgScheduleController(UserScheduleManager organizer, TalkManager talkManager,
-                                 MainMenuController mainMenuController){
-        super(organizer, talkManager, mainMenuController);
+                                 MainMenuController mainMenuController, HashMap<Talk, SignUpAttendeesManager>
+                                         signUpAttendeesManager){
+        super(organizer, talkManager, mainMenuController, signUpAttendeesManager);
         this.talkManager = talkManager;
         this.mainMenuController = mainMenuController;
     }
 
-    public boolean requestTalks(String title, String talkId,  String speakerEmail, String roomName, LocalDateTime time) {
-        return this.talkManager.createTalk(title, talkId, speakerEmail, roomName, time);
+    public boolean requestTalks(String title, String speakerEmail, String roomName, LocalDateTime time) {
+        //return this.talkManager.createTalk(title, talkId, speakerEmail, roomName, time);
+        return true;
     }
 
     //there's also a createRoom in RoomStorage with the parameter capacity
@@ -33,10 +33,22 @@ public class OrgScheduleController extends UserScheduleController implements Obs
     }
 
     //can't put anything here since speakerStorage hasn't been made
-    public boolean createSpeaker() {
-        return false;
+    public boolean requestSpeaker(String name, String password, String email) {
+        return this.userStorage.createUser("Speaker", name, password, email);
     }
-
+//    public void run(){
+//        Presenter presenter = new Presenter();
+//        Scanner scan = new Scanner(System.in);
+//        boolean doContinue = true;
+//        while (doContinue){
+//            int command = scan.nextInt();
+//            if (command == 1){
+//
+//            }
+//            else if (command == )
+//        }
+//
+//    }
     @Override
     public void update(Observable o, Object arg) {
         if(arg instanceof RoomStorage){
