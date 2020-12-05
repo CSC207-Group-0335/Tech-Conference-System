37032a7e7a5e2fafc228b01095c0ccf92f916794
Jolie Zhou <jolie.zhou@mail.utoronto.ca>
2020-12-05 17:22:32 -0500

    changed requests & request methods to HashMap, added deny request methods

--- phase2/src/UserLogin/Attendee.java
+++ phase2/src/UserLogin/Attendee.java
@@ -1,6 +1,7 @@
 package UserLogin;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A class that represents an Attendee, a specific type of User that attends the conference and can sign up to
@@ -11,7 +12,7 @@ public class Attendee extends User {
     public boolean VIP;
     //make it a list of strings for multiple requests?
     // strings are in format of "request, status", with status only being one of two "pending" or "addressed"
-    public ArrayList<String> requests;
+    public HashMap<String, String> requests;
 
     /**
      * A constructor for an Attendee
@@ -23,13 +24,13 @@ public class Attendee extends User {
     public Attendee(String name, String password, String email) {
         super(name, password, email);
         this.VIP = false;
-        this.requests = new ArrayList<>();
+        this.requests = new HashMap<>();
     }
 
     public Attendee(String name, String password, String email, boolean VIP) {
         super(name, password, email);
         this.VIP = VIP;
-        this.requests = new ArrayList<>();
+        this.requests = new HashMap<>();
     }
 
 
@@ -43,49 +44,116 @@ public class Attendee extends User {
         return "Attendee";
     }
 
+    /**
+     * Returns true if and only if this user is VIP.
+     *
+     * @return a Boolean representing whether or not this user is VIP
+     */
+
     public boolean getVIPStatus() {
         return this.VIP;
     }
 
-    public ArrayList<String> getRequests() {
+    /**
+     * Returns requests and their statuses (pending, approved, or rejected).
+     *
+     * @return a HashMap containing requests and their statuses
+     */
+
+    public HashMap<String, String> getRequests() {
         return this.requests;
     }
 
+    /**
+     * Returns true if and only if the request has been set.
+     *
+     * @param request a String representing the request
+     * @return a boolean representing whether or not the request has been set
+     */
+
     public boolean setRequests(String request) {
-        String toAdd = request + ", pending";
-        this.requests.add(toAdd);
+        requests.put(request, "pending");
         return true;
     }
+
+    /**
+     * Returns true if and only if the requests have been set.
+     *
+     * @param requests an ArrayList containing multiple requests
+     * @return a boolean representing whether or not the requests have been set
+     */
 
     public boolean setRequests(ArrayList<String> requests) {
         for (String req : requests) {
-            String toAdd = req + ", pending";
-            this.requests.add(toAdd);
+            this.requests.put(req, "pending");
         }
         return true;
     }
 
+    /**
+     * Returns true if the request exists and has been approved. Returns false otherwise.
+     *
+     * @param request a String representing the request
+     * @return a boolean representing whether or not the request has been successfully approved
+     */
+
     public boolean requestComplete(String request) {
-        int i = 0;
-        for (String req : this.requests) {
-            if (request.equals(req)) {
-                int indexOfComma = req.indexOf(",");
-                String content = req.substring(0, indexOfComma);
-                String toAdd = content + ", addressed";
-                this.requests.set(i, toAdd);
+        if (requests.containsKey(request)) {
+            requests.put(request, "addressed");
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns true if each request exists and has been approved. Returns false otherwise.
+     *
+     * @param requests an ArrayList containing multiple requests
+     * @return a boolean representing whether or not the requests have been successfully approved
+     */
+
+    public boolean requestCompleteAll(ArrayList<String> requests) {
+        for (String req : requests) {
+            if (!this.requests.containsKey(req)) {
+                return false;
             }
-            i++;
+            else {
+                this.requests.put(req, "addressed");
+            }
         }
         return true;
     }
-    public boolean requestCompleteAll(String request) {
-        int i = 0;
-        for (String req : this.requests) {
-            int indexOfComma = req.indexOf(",");
-            String content = req.substring(0, indexOfComma);
-            String toAdd = content + ", addressed";
-            this.requests.set(i, toAdd);
-            i++;
+
+    /**
+     * Returns true if the request exists and has been refused. Returns false otherwise.
+     *
+     * @param request a String representing the request
+     * @return a boolean representing whether or not the request has been successfully refused
+     */
+
+    public boolean requestDeny(String request) {
+        if (requests.containsKey(request)) {
+            requests.put(request, "rejected");
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Returns true if each request exists and has been denied. Returns false otherwise.
+     *
+     * @param requests an ArrayList containing multiple requests
+     * @return a boolean representing whether or not the requests have been successfully refused
+     */
+
+    public boolean requestDenyAll(ArrayList<String> requests) {
+        for (String req : requests) {
+            if (!this.requests.containsKey(req)) {
+                return false;
+            }
+            else {
+                this.requests.put(req, "refused");
+            }
         }
         return true;
     }
