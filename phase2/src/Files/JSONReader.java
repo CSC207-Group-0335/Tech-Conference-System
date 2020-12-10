54a609cf9f7f78ef5b92129601325cffef509827
Connor Burns <connor.burns@mail.utoronto.ca>
2020-12-10 03:24:25 -0500

    finished reader javadocs

--- phase2/src/Files/JSONReader.java
+++ phase2/src/Files/JSONReader.java
@@ -6,6 +6,12 @@ import java.io.FileReader;
 
 public class JSONReader{
 
+    /**
+     * Method which returns a JSON Parser..
+     * @param json The string location of the JSON.
+     * @return A JSON Parser.
+     * @throws Exception
+     */
     public Object readJson(String json) throws Exception {
         FileReader reader = new FileReader(json);
         JSONParser jsonParser = new JSONParser();
